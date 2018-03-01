ec1a6abe6bbf3c5b635b0e0611a726960ae7679d
Husam Burhan <husam.burhan@gmaili.com>
2018-03-01 15:28:45 +0300

    Ver 1.1
    Basic Operations

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     String leftOperand = "";
     String rightOperand = "";
-    String currentOperation = ""; //1: +, 2: -, 3: *, 4: /
+    String currentOperation = "";
     boolean clearScreen = false;
 
     @Override
@@ -20,7 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sqClicked(View view) {
+    public void minusFlipClicked(View view) {
+        if (currentOperation == "" && leftOperand != "") {
+            double leftOp = Double.parseDouble(leftOperand);
+            leftOp = leftOp * -1;
+
+            leftOperand = String.valueOf(leftOp);
+
+            displayOnScreen(formatIntNumber(leftOp));
+        } else if (currentOperation != "" && rightOperand != "") {
+            double rightOp = Double.parseDouble(rightOperand);
+            rightOp = rightOp * -1;
+
+            rightOperand = String.valueOf(rightOp);
+
+            displayOnScreen(formatIntNumber(rightOp));
+        }
     }
 
     public void acClicked(View view) {
@@ -91,11 +106,19 @@ public class MainActivity extends AppCompatActivity {
         displayOnScreen("0");
     }
 
+    private String formatIntNumber(double number) {
+
+        if (number == Math.floor(number)) {
+            return String.valueOf((int) number);
+        } else {
+            return String.valueOf(number);
+        }
+    }
+
     public void equalClicked(View view) {
         double leftOp = Double.parseDouble(leftOperand);
         double rightOp = Double.parseDouble(rightOperand);
         double result = 0;
-
 
         if (currentOperation == "+") {
             result = leftOp + rightOp;
@@ -113,17 +136,14 @@ public class MainActivity extends AppCompatActivity {
                 leftOperand = "";
                 rightOperand = "";
                 currentOperation = "";
+
+                return;
             }
         } else {
             return;
         }
 
-        if (result == Math.floor(result)) {
-            displayOnScreen(String.valueOf((int) result));
-        } else {
-            displayOnScreen(String.valueOf(result));
-        }
-
+        displayOnScreen(formatIntNumber(result));
 
         leftOperand = String.valueOf(result);
         rightOperand = "";
